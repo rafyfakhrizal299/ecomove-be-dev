afefb7a135fc5594981e95e314089752536e70d5
RafyFH <rafyfakhrizal299@gmail.com>
2025-07-22 22:52:10 +0700

    index.js

--- dev.js
+++ dev.js
@@ -1,6 +1,9 @@
 import app from './src/app.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const PORT = process.env.PORT || 3000
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Local server running at http://localhost:${PORT}`)
