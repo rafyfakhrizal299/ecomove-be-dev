8d93d94ca601107993ce7a3b54a029cc53efb91d
RafyFH <rafyfakhrizal299@gmail.com>
2025-07-29 21:35:04 +0700

    skip transaction route

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/auth', authRoutes)
-app.use('/transaction', transactionRoutes)
+// app.use('/transaction', transactionRoutes)
 app.use('/address', addressRoutes)
 
 export default app
