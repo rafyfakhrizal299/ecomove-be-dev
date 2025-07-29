ac5dba11086c82e719a87776c893a130ea2f35e7
RafyFH <rafyfakhrizal299@gmail.com>
2025-07-29 21:36:17 +0700

    tertinggal

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import transactionRoutes from './routes/transaction.route.js'
+// import transactionRoutes from './routes/transaction.route.js'
 import authRoutes from './routes/auth.route.js'
 import addressRoutes from './routes/address.route.js'
 
