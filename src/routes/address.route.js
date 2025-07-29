333739c236a9dc1d7937f9033d1ff07db7fe5298
RafyFH <rafyfakhrizal299@gmail.com>
2025-07-29 21:43:28 +0700

    get address by userid

--- src/routes/address.route.js
+++ src/routes/address.route.js
@@ -12,6 +12,8 @@ import adminOnly from '../middlewares/admin.middleware.js'
 
 const router = express.Router()
 
+router.get('/get-by-user/:userId', getSavedAddressesByUser)
+
 router.use(authMiddleware, adminOnly)
 
 router.get('/', getAllSavedAddresses)
@@ -19,7 +21,6 @@ router.get('/:id', getSavedAddressById)
 router.post('/', createSavedAddress)
 router.put('/:id', updateSavedAddress)
 router.delete('/:id', deleteSavedAddress)
-router.get('/get-by-user/:userId', getSavedAddressesByUser)
 
 
 export default router
