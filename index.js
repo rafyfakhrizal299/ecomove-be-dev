ec999c69ee066119da4fd8ae28971e5e9fd6ff62
Pradeva <90541409+Pradeva@users.noreply.github.com>
2025-10-05 10:41:12 +0700

    .well-known

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import app from './src/app.js'
 export const handler = serverless(app)
 
 // Kalau dijalankan langsung di lokal → start server biasa -
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Local server running at http://localhost:${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   const PORT = process.env.PORT || 3000;
+//   app.listen(PORT, () => {
+//     console.log(`Local server running at http://localhost:${PORT}`);
+//   });
+// }
