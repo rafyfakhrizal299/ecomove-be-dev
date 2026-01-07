fdc67fafbb2b2368e64094a40f0847fb9cef9142
rafyfakhrizal299 <rafyfakhrizal299@gmail.com>
2026-01-07 22:00:16 +0700

    fixing prefix

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -9,22 +9,27 @@ import {
   dashboardController,
   cancelTransactionReceiver
 } from "../controllers/transaction.controller.js";
-import authMiddleware from '../middlewares/auth.middleware.js'
-import adminOnly from '../middlewares/admin.middleware.js'
+import authMiddleware from '../middlewares/auth.middleware.js';
+import adminOnly from '../middlewares/admin.middleware.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
+// ===== PUBLIC (LOGIN REQUIRED) =====
 router.post("/", createTransaction);
 router.get("/", getAllTransactions);
 router.get("/page", getTransactions);
-router.get("/:id", getTransactionById);
-router.delete("/:id", deleteTransaction);
-router.patch('/receivers/:id/cancel', cancelTransactionReceiver)
 
-router.use(adminOnly);
-router.get("/dashboard", dashboardController);
-router.put("/:id", updateTransaction);
+// receiver
+router.patch("/receivers/:id/cancel", cancelTransactionReceiver);
+
+// ===== ADMIN ONLY =====
+router.get("/dashboard", adminOnly, dashboardController);
+router.put("/:id(eco[0-9]+)", adminOnly, updateTransaction);
+
+// ===== BY ID (eco<number>) =====
+router.get("/:id(eco[0-9]+)", getTransactionById);
+router.delete("/:id(eco[0-9]+)", deleteTransaction);
 
 export default router;
