b283c10b6e79a7da64ac3e618959a686706813c9
rafyfakhrizal <rafy.fakhrizal@cxrus.com>
2026-01-13 13:48:40 +0700

    controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -179,7 +179,7 @@ export const getTransactions = async (req, res) => {
     const search = req.query.search || null;
 
     const filters = {
-      status: req.query.status || null,
+      status: req.query.filters.status || null,
     };
 
     const sortDate = req.query.sortDate || 'desc';
